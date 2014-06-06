dee5ef7f4712f50edd3caab5f8018cca7e3042f6
Christian Rothmann <christian.rothmann@gmail.com>
2014-06-06 19:10:30 +0200

    add plot4.R

--- plot4.R
+++ plot4.R
@@ -1,8 +1,8 @@
-# R code file that constructs the PLOT 1 for the Projet 1. The Code File include
+# R code file that constructs the PLOT 4 for the Projet 1. The Code File include
 # the code for reading the data so that the plot can be fully reproduced. It 
 # also creates the PNG file.
 
-file.png = "plot1.png"
+file.png = "plot4.png"
 filename = "household_power_consumption.txt"
 
 # check if the data is in the current working directory. If not, download and unzip file.
@@ -34,6 +34,17 @@ dataf$datetime <- paste(dataf$Date, dataf$Time)
 dataf$datetime <- strptime(dataf$datetime, "%d/%m/%Y %H:%M:%S")
 dataf$datetime <- as.Date(dataf$datetime)
 
+# get and save aspects of the LOCALE for the R process for my case i am in
+# AUSTRIA and use the locale "German_Austria.1252", so the x axes labels are 
+# "Do" instead of "Thu" ("Donnerstag" instead of "Thursday")  / "Fr" for "Fri" /
+# "Sa" for "Sat"
+mylocation <- Sys.getlocale("LC_TIME")
+
+# set Time Location to C locale (which is the default for the C language and
+# reflects North-American usage) so it is in North-American Englisch and from
+# now on it appears Thu/Fri/Sat
+Sys.setlocale("LC_TIME", "C")
+
 # Open PNG device; create 'plot1.png' in my working directory
 # uses cairographics' PNG backend which will never use a palette and normally
 # creates a larger 32-bit ARGB file - this may work better for specialist uses
@@ -41,12 +52,19 @@ dataf$datetime <- as.Date(dataf$datetime)
 png(file = file.png, width = 480, height = 480,  type = "cairo-png", 
     bg = "transparent")
 
-# ready for PLOT 1
+# ready for PLOT 4
 par(mfrow = c(1,1))
-hist(dataf$Global_active_power,xlab = "Global Active Power (kilowatts)", 
-     col = "red", main = "Global Active Power")
+with(dataf, plot(datetime, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n"))
+with(dataf, lines(datetime, Sub_metering_1))
+with(dataf, lines(datetime, Sub_metering_2, col = "red"))
+with(dataf, lines(datetime, Sub_metering_3, col = "blue"))
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+       lty = c(1,1,1), col = c("black","red", "blue"))
 
 # Close the PNG file device and write file
 dev.off()
+
+# switch back to the original LOCALE for date/time processing
+Sys.setlocale("LC_TIME", mylocation)
 
 cat(file.png, "created in working directory.\n")
